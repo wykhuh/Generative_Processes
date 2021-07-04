4d6ca7dc461dca57e9c233a7b200f55a92672427
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2021-07-04 08:23:39 -0700

    add initial files

---
+++ readme.md
@@ -0,0 +1,1 @@
+https://www.youtube.com/playlist?list=PLLx7jIm38p9m6n8X01Sz2kQaSC9U6_6u5
---
+++ template/sketch.js
@@ -0,0 +1,19 @@
+// ------------------------------------------------------------------
+// Code placed in setup() will run once at the beginning
+function setup() {
+  // Create a new canvas to match the browser size
+  createCanvas(windowWidth, windowHeight);
+}
+
+// ------------------------------------------------------------------
+// On window resize, update the canvas size
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+// ------------------------------------------------------------------
+// Main render loop - code placed in draw() will repeat over and over
+function draw() {
+  // Place your drawing code here
+}
+
