d1fa1cdb3365398c753d10774efed62a8ba28dbc
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2021-07-04 13:06:22 -0700

    update template to work with npm p5 and vite

--- readme.md
+++ readme.md
@@ -1,8 +1,20 @@
 https://www.youtube.com/playlist?list=PLLx7jIm38p9m6n8X01Sz2kQaSC9U6_6u5
 
-
-generate new files
+## generate new files
 
 ```{bash}
 ./generate_files.sh <new directory>
+```
+
+## start server
+
+install vite
+
+```{bash}
+npm install -g vite
+```
+
+```{bash}
+cd <directory>
+vite
 ```
--- template/sketch.js
+++ template/sketch.js
@@ -1,19 +1,17 @@
-// ------------------------------------------------------------------
-// Code placed in setup() will run once at the beginning
-function setup() {
-  // Create a new canvas to match the browser size
-  createCanvas(windowWidth, windowHeight);
-}
+import p5 from "p5";
 
-// ------------------------------------------------------------------
-// On window resize, update the canvas size
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-}
+const sketch = (s) => {
+  s.setup = () => {
+    s.createCanvas(s.windowWidth, s.windowHeight);
+  };
 
-// ------------------------------------------------------------------
-// Main render loop - code placed in draw() will repeat over and over
-function draw() {
-  // Place your drawing code here
-}
+  s.windowResized = () => {
+    s.resizeCanvas(s.windowWidth, s.windowHeight);
+  };
 
+  s.draw = () => {
+    s.circle(30, 30, 30);
+  };
+};
+
+const sketchInstance = new p5(sketch);
