c79692d474e39238fec7a38ca79d49f213fbe2dc
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2021-07-04 18:09:18 -0700

    2.2 control alpha of line using gui

--- 2.2_dancing_lines/sketch.js
+++ 2.2_dancing_lines/sketch.js
@@ -3,6 +3,7 @@ import * as dat from "dat.gui";
 const sketch = (s) => {
   let settings = {
     res: 0.01,
+    alpha: 128,
   };
   let gui;
 
@@ -12,6 +13,7 @@ const sketch = (s) => {
 
     gui = new dat.GUI();
     gui.add(settings, "res", 0.001, 0.02);
+    gui.add(settings, "alpha", 5, 255);
   };
 
   s.windowResized = () => {
@@ -20,20 +22,16 @@ const sketch = (s) => {
   };
 
   s.draw = () => {
-    s.background(0);
-    s.stroke(255);
-    s.strokeWeight(5);
+    // s.background(0);
+    s.stroke(255, settings.alpha);
+    s.strokeWeight(1);
 
+    // add constant to noise so that noise for x and y will return a different value
     let x1 = s.noise(s.frameCount * settings.res) * s.width;
-    // add constant so that noise for x and y will return a different value
     let y1 = s.noise(100 + s.frameCount * settings.res) * s.height;
-
-    s.point(x1, y1);
 
     let x2 = s.noise(200 + s.frameCount * settings.res) * s.width;
     let y2 = s.noise(300 + s.frameCount * settings.res) * s.height;
-
-    s.point(x2, y2);
 
     s.line(x1, y1, x2, y2);
   };
