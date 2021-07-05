4024c0137aa1b468b603fa26a2b30c1244ac7c27
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2021-07-05 16:14:12 -0700

    4.3 draw grid

--- 4.3_perlin_field/sketch.js
+++ 4.3_perlin_field/sketch.js
@@ -2,7 +2,7 @@ import p5 from "p5";
 
 const sketch = (s) => {
   let highway = "highway.jpg";
-  let grid = 160;
+  let grid = 20;
   let img;
 
   s.setup = () => {
@@ -23,12 +23,18 @@ const sketch = (s) => {
 
   s.draw = () => {
     s.background(0);
+    s.stroke(255);
+    s.fill(0);
+
+    s.rectMode(s.CENTER);
 
     for (let x = 0; x < s.width; x += grid) {
       for (let y = 0; y < s.height; y += grid) {
         s.push();
         // set origin to center each cell
         s.translate(x + grid / 2, y + grid / 2);
+
+        s.rect(0, 0, grid);
 
         s.pop();
       }
