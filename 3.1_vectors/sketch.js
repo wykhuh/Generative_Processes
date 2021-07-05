7a71e701283e78707a244deb7a122733a4aa1d4c
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2021-07-04 22:42:40 -0700

    3.1 sorte position, velocity and acceleration in agent object

--- 3.1_vectors/sketch.js
+++ 3.1_vectors/sketch.js
@@ -1,9 +1,7 @@
 import p5 from "p5";
 
 const sketch = (s) => {
-  let position = new p5.Vector();
-  let velocity = new p5.Vector();
-  let acceleration = new p5.Vector();
+  let agent;
 
   let easing = 0.1; // a value to temper acceleration
   let damping = 0.5; // value to temper velocity; high
@@ -14,13 +12,7 @@ const sketch = (s) => {
     // top left corner of rectangle
     s.rectMode(s.CENTER);
 
-    position.x = s.width / 2;
-    position.y = s.height / 2;
-
-    velocity.x = s.random(-1, 1);
-    velocity.y = s.random(-1, 1);
-
-    s.background(0);
+    agent = createAgent();
   };
 
   s.windowResized = () => {
@@ -30,35 +22,44 @@ const sketch = (s) => {
   s.draw = () => {
     s.noFill();
     s.stroke(255);
+    s.background(0);
 
     // set acceleration to zero
-    acceleration.mult(0);
+    agent.acceleration.mult(0);
 
     // vector for mouse position
     let target = new p5.Vector(s.mouseX, s.mouseY);
 
     // subtract position vector from current mouse vector to calculate the
     // needed vector in order to make square follow mouse
-    let diff = target.sub(position);
+    let diff = target.sub(agent.position);
     // reduce vector
     diff.mult(easing);
 
     // add vectors
-    acceleration.add(diff);
-    velocity.add(acceleration);
-    velocity.mult(damping);
-    position.add(velocity);
+    agent.acceleration.add(diff);
+    agent.velocity.add(agent.acceleration);
+    agent.velocity.mult(damping);
+    agent.position.add(agent.velocity);
 
     s.push();
     // to rotate the square, we need to move origin to center of square
-    s.translate(position.x, position.y);
+    s.translate(agent.position.x, agent.position.y);
     // rotate square to match angle of the velocity
-    s.rotate(velocity.heading());
+    s.rotate(agent.velocity.heading());
     s.rect(0, 0, 50);
     s.pop();
 
     // drawVector();
   };
+
+  function createAgent() {
+    return {
+      position: new p5.Vector(),
+      velocity: new p5.Vector(),
+      acceleration: new p5.Vector(),
+    };
+  }
 
   function drawVector() {
     s.line(0, 0, position.x, position.y);
