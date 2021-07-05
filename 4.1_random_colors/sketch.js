bbf960cce470e7db142b85817d465a8f0735f993
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2021-07-05 09:41:21 -0700

    4.1 create grid

---
+++ 4.1_random_colors/sketch.js
@@ -0,0 +1,29 @@
+import p5 from "p5";
+
+const sketch = (s) => {
+  let grid = 40;
+  s.setup = () => {
+    s.createCanvas(s.windowWidth, s.windowHeight);
+  };
+
+  s.windowResized = () => {
+    s.resizeCanvas(s.windowWidth, s.windowHeight);
+  };
+
+  s.draw = () => {
+    s.background(0);
+
+    s.rectMode(s.CENTER);
+    for (let x = 0; x < s.width; x += grid) {
+      for (let y = 0; y < s.height; y += grid) {
+        s.push();
+        // set origin to center each cell
+        s.translate(x + grid / 2, y + grid / 2);
+        s.rect(0, 0, grid);
+        s.pop();
+      }
+    }
+  };
+};
+
+new p5(sketch);
