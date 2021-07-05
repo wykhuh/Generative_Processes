115794be5dfd65c499e8a37baeeb29b533ad4b29
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2021-07-05 02:50:52 -0700

    3.1 refactor code into separate functions

--- 3.1_vectors/sketch.js
+++ 3.1_vectors/sketch.js
@@ -24,9 +24,41 @@ const sketch = (s) => {
     s.stroke(255);
     s.background(0);
 
+    move(agent);
+    followMouse(agent);
+    render(agent);
+  };
+
+  // create a moving item
+  function createAgent() {
+    return {
+      position: new p5.Vector(),
+      velocity: new p5.Vector(),
+      acceleration: new p5.Vector(),
+    };
+  }
+
+  // draw the agent
+  function render(agent) {
+    s.push();
+    // to rotate the agent, we need to move origin to center of agent
+    s.translate(agent.position.x, agent.position.y);
+    // rotate agent to match angle of the velocity
+    s.rotate(agent.velocity.heading());
+    s.rect(0, 0, 50);
+    s.pop();
+  }
+
+  function move(agent) {
+    agent.velocity.add(agent.acceleration);
+    agent.velocity.mult(damping);
+    agent.position.add(agent.velocity);
+
     // set acceleration to zero
     agent.acceleration.mult(0);
+  }
 
+  function followMouse(agent) {
     // vector for mouse position
     let target = new p5.Vector(s.mouseX, s.mouseY);
 
@@ -38,37 +70,6 @@ const sketch = (s) => {
 
     // add vectors
     agent.acceleration.add(diff);
-    agent.velocity.add(agent.acceleration);
-    agent.velocity.mult(damping);
-    agent.position.add(agent.velocity);
-
-    s.push();
-    // to rotate the square, we need to move origin to center of square
-    s.translate(agent.position.x, agent.position.y);
-    // rotate square to match angle of the velocity
-    s.rotate(agent.velocity.heading());
-    s.rect(0, 0, 50);
-    s.pop();
-
-    // drawVector();
-  };
-
-  function createAgent() {
-    return {
-      position: new p5.Vector(),
-      velocity: new p5.Vector(),
-      acceleration: new p5.Vector(),
-    };
-  }
-
-  function drawVector() {
-    s.line(0, 0, position.x, position.y);
-
-    // draw velocity vector
-    s.push();
-    s.translate(position.x, position.y);
-    s.line(0, 0, velocity.x * 20, velocity.y * 20);
-    s.pop();
   }
 };
 
