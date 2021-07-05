1c5b5a656d9dd6bc3750ddaa73f95d43a2e7cca0
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2021-07-05 15:05:05 -0700

    4.2 show subset of image based on mouse position

--- 4.2_image_grid/sketch.js
+++ 4.2_image_grid/sketch.js
@@ -25,13 +25,17 @@ const sketch = (s) => {
     s.background(0);
 
     s.imageMode(s.CENTER);
+
+    let sx = s.map(s.mouseX, 0, s.width, 0, img.width);
+    let sy = s.map(s.mouseY, 0, s.height, 0, img.height);
+
     for (let x = 0; x < s.width; x += grid) {
       for (let y = 0; y < s.height; y += grid) {
         s.push();
         // set origin to center each cell
         s.translate(x + grid / 2, y + grid / 2);
 
-        s.image(img, 0, 0, grid, grid);
+        s.image(img, 0, 0, grid, grid, sx, sy, grid, grid);
 
         s.pop();
       }
