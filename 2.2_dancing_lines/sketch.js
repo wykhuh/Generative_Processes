3c9f3f77733ef96df69a77af4315b724bb908b93
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2021-07-04 18:20:53 -0700

    2.2 only draw x number of lines that is set by gui

--- 2.2_dancing_lines/sketch.js
+++ 2.2_dancing_lines/sketch.js
@@ -4,8 +4,10 @@ const sketch = (s) => {
   let settings = {
     res: 0.01,
     alpha: 128,
+    nFrames: 50,
   };
   let gui;
+  let recorder = [];
 
   s.setup = () => {
     s.createCanvas(s.windowWidth, s.windowHeight);
@@ -14,6 +16,7 @@ const sketch = (s) => {
     gui = new dat.GUI();
     gui.add(settings, "res", 0.001, 0.02);
     gui.add(settings, "alpha", 5, 255);
+    gui.add(settings, "nFrames", 5, 500);
   };
 
   s.windowResized = () => {
@@ -22,19 +25,37 @@ const sketch = (s) => {
   };
 
   s.draw = () => {
-    // s.background(0);
+    s.background(0);
     s.stroke(255, settings.alpha);
     s.strokeWeight(1);
 
     // add constant to noise so that noise for x and y will return a different value
     let x1 = s.noise(s.frameCount * settings.res) * s.width;
     let y1 = s.noise(100 + s.frameCount * settings.res) * s.height;
-
     let x2 = s.noise(200 + s.frameCount * settings.res) * s.width;
     let y2 = s.noise(300 + s.frameCount * settings.res) * s.height;
 
-    s.line(x1, y1, x2, y2);
+    let frame = {
+      x1,
+      y1,
+      x2,
+      y2,
+    };
+    recorder.push(frame);
+
+    // remove lines when there are too many lines or nFrames changes
+    while (recorder.length > settings.nFrames) {
+      recorder.shift();
+    }
+    playback();
   };
+
+  // draw line for values in recorder
+  function playback() {
+    for (let frame of recorder) {
+      s.line(frame.x1, frame.y1, frame.x2, frame.y2);
+    }
+  }
 };
 
 const sketchInstance = new p5(sketch);
