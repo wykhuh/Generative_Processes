5ee97a90e6b4428e2eaf68a0d623d32799bfb2f5
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2021-07-04 19:43:41 -0700

    3.1 draw moving square with velocity vector

--- 3.1_vectors/sketch.js
+++ 3.1_vectors/sketch.js
@@ -2,6 +2,7 @@ import p5 from "p5";
 
 const sketch = (s) => {
   let position = new p5.Vector();
+  let velocity = new p5.Vector();
 
   s.setup = () => {
     s.createCanvas(s.windowWidth, s.windowHeight);
@@ -9,8 +10,11 @@ const sketch = (s) => {
     // top left corner of rectangle
     s.rectMode(s.CENTER);
 
-    position.x = 100;
-    position.y = 200;
+    position.x = s.width / 2;
+    position.y = s.height / 2;
+
+    velocity.x = s.random(-1, 1);
+    velocity.y = s.random(-1, 1);
   };
 
   s.windowResized = () => {
@@ -21,6 +25,9 @@ const sketch = (s) => {
     s.background(0);
     s.noFill();
     s.stroke(255);
+
+    position.x += velocity.x;
+    position.y += velocity.y;
 
     s.rect(position.x, position.y, 50);
   };
