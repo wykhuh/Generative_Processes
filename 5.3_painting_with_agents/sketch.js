3759a846b3ce09fe71d0677a4189b929b8db24a4
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2021-07-08 08:15:10 -0700

    5.3 draw agents as lines

--- 5.3_painting_with_agents/sketch.js
+++ 5.3_painting_with_agents/sketch.js
@@ -56,7 +56,7 @@ const sketch = (s) => {
     s.translate(agent.pos.x, agent.pos.y);
     // rotate the origin to same angle as the agent velocity
     s.rotate(agent.vel.heading());
-    s.point(0, 0);
+    s.line(0, 0, 20, 0);
     s.pop();
   }
 
