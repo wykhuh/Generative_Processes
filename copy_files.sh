b547ea357f02679bfdc8630b5062b5924e1e36e0
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2021-07-08 08:00:13 -0700

    add new directory to index.html and vite.config.js when copying or generating directory

--- copy_files.sh
+++ copy_files.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 # 1. copy "template" into a new directory
-cp -r ./template $1
+cp -r $1 $2
 
 # 2. update vite.config.js and index.html with new directory
 # https://unix.stackexchange.com/questions/121161/how-to-insert-text-after-a-certain-string-in-a-file
 # https://stackoverflow.com/questions/7573368/in-place-edits-with-sed-on-os-x
 
 sed -i '' "/insert text -->/i\\
-        \"$1\": resolve(__dirname, \"$1/index.html\"),
+        \"$2\": resolve(__dirname, \"$2/index.html\"),
 " vite.config.js
 
 sed -i '' "/insert text -->/i\\
-      <li><a href=\"./$1\">$1</a></li>
+      <li><a href=\"./$2\">$2</a></li>
 " index.html
+
