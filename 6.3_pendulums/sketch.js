b0745a04433b9e013b249005bfc5fd1246073ef8
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2021-07-08 18:43:39 -0700

    6.3 draw pendulum

--- 6.3_pendulums/sketch.js
+++ 6.3_pendulums/sketch.js
@@ -5,8 +5,17 @@ const sketch = (s) => {
   let masterVolume = -10;
   let ready = false;
 
+  let lfo;
+  let meter;
+
   s.setup = () => {
     s.createCanvas(s.windowWidth, s.windowHeight);
+
+    lfo = new Tone.LFO(0.85);
+    lfo.start();
+    meter = new Tone.Meter();
+    meter.normalRange = true; // return value from 0 to 1
+    lfo.connect(meter);
   };
 
   s.windowResized = () => {
@@ -17,6 +26,13 @@ const sketch = (s) => {
     s.background(0);
 
     if (ready) {
+      let position = 0.5 - meter.getValue(0); // return value -0.5  to 0.5
+      let x = s.map(position, -0.5, 0.5, 100, s.width - 100);
+
+      s.fill(255);
+      s.stroke(255);
+      s.line(x, s.height / 2, s.width / 2, 0);
+      s.ellipse(x, s.height / 2, 25, 25);
     } else {
       s.fill(255);
       s.noStroke();
