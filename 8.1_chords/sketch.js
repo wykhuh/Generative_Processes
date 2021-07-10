1a74c2277a32bf1ea3599a63cf821d31564bc286
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2021-07-10 16:54:05 -0700

    8.1 generate all the chords for the scale

--- 8.1_chords/sketch.js
+++ 8.1_chords/sketch.js
@@ -1,16 +1,27 @@
 import p5 from "p5";
 import * as Tone from "tone";
-import { Scale } from "@tonaljs/tonal";
+import { Scale, Note } from "@tonaljs/tonal";
 
 const sketch = (s) => {
   let masterVolume = -10;
   let ready = false;
   let scaleNotes = Scale.get("C4 major").notes;
+  let chords = [];
 
   let poly;
 
   s.setup = () => {
     s.createCanvas(s.windowWidth, s.windowHeight);
+
+    // generate all the chords in the scale
+    for (let i = 0; i < scaleNotes.length; i++) {
+      let chord = [];
+      chord[0] = mapNote(i, scaleNotes);
+      chord[1] = mapNote(i + 2, scaleNotes);
+      chord[2] = mapNote(i + 4, scaleNotes);
+      chord[3] = mapNote(i + 6, scaleNotes);
+      chords.push(chord);
+    }
   };
 
   s.windowResized = () => {
@@ -35,7 +46,7 @@ const sketch = (s) => {
       initAudio();
     }
 
-    poly.triggerAttackRelease(["C4", "E4", "G4"], "1m");
+    poly.triggerAttackRelease(chords[1], "1m");
   };
 
   function initAudio() {
@@ -44,6 +55,21 @@ const sketch = (s) => {
       envelope: { attack: 1, release: 2 },
     });
     poly.toDestination();
+  }
+
+  function mapNote(noteNumber, scale) {
+    let numberNotes = scale.length;
+    let i = modulo(noteNumber, numberNotes);
+    let note = scale[i];
+
+    let zeroOctave = Note.octave(scale[0]);
+    let noteOctave = zeroOctave + s.floor(noteNumber / numberNotes);
+    let noteName = Note.pitchClass(note);
+    return noteName + noteOctave;
+  }
+
+  function modulo(n, m) {
+    return ((n % m) + m) % m;
   }
 };
 
