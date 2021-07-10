dd042186a0f5d845887e903384d0dcdc84f08042
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2021-07-10 06:39:26 -0700

    7.1 display note name based on mouse position

--- 7.1_note_mapping/sketch.js
+++ 7.1_note_mapping/sketch.js
@@ -1,12 +1,15 @@
 import p5 from "p5";
 import * as Tone from "tone";
+import { Scale } from "@tonaljs/tonal";
 
 const sketch = (s) => {
   let masterVolume = -10;
   let ready = false;
+  let scale;
 
   s.setup = () => {
     s.createCanvas(s.windowWidth, s.windowHeight);
+    scale = Scale.get("C4 major").notes;
   };
 
   s.windowResized = () => {
@@ -17,6 +20,14 @@ const sketch = (s) => {
     s.background(0);
 
     if (ready) {
+      let noteNumber = s.floor(s.map(s.mouseX, 0, s.width, 0, scale.length));
+      let note = scale[noteNumber];
+
+      s.fill(255);
+      s.noStroke();
+      s.textAlign(s.CENTER, s.CENTER);
+      s.textSize(100);
+      s.text(note, s.width / 2, s.height / 2);
     } else {
       s.fill(255);
       s.noStroke();
@@ -28,8 +39,11 @@ const sketch = (s) => {
   s.mousePressed = () => {
     if (!ready) {
       ready = true;
+      initAudio();
     }
   };
+
+  function initAudio() {}
 };
 
 new p5(sketch);
