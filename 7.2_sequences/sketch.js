4c437227538f2c00eb7991a96a354eaa118eee6e
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2021-07-10 11:15:35 -0700

    7.2 move code to Track class

--- 7.2_sequences/sketch.js
+++ 7.2_sequences/sketch.js
@@ -6,11 +6,10 @@ const sketch = (s) => {
   let masterVolume = -10;
   let ready = false;
   let scale;
-  let synth;
-  let currentNote;
 
-  let pattern;
   let sequence = [0, 2, 4, 6]; // array of indicies for scale
+
+  let track;
 
   s.setup = () => {
     s.createCanvas(s.windowWidth, s.windowHeight);
@@ -29,7 +28,7 @@ const sketch = (s) => {
       s.noStroke();
       s.textAlign(s.CENTER, s.CENTER);
       s.textSize(100);
-      s.text(currentNote, s.width / 2, s.height / 2);
+      s.text(track.currentNote, s.width / 2, s.height / 2);
     } else {
       s.fill(255);
       s.noStroke();
@@ -62,29 +61,7 @@ const sketch = (s) => {
   }
 
   function initAudio() {
-    synth = new Tone.Synth();
-    synth.toDestination();
-    synth.volume.value = masterVolume;
-
-    // Pattern cycles through the passed in sequence
-    pattern = new Tone.Pattern(
-      (time, index) => {
-        let note = mapNote(sequence[index], scale);
-        // time is a schedule time in the future. Passing time to synth ensures
-        // the note is played on beat.
-        synth.triggerAttackRelease(note, "8n", time);
-        currentNote = note;
-      },
-      // to allow us to change the sequence while the program is running, we
-      // pass an array of indicies to Tone.Pattern()
-      // create an array starting at 0, that is same length as sequence.
-      Array.from(sequence.keys()),
-      "up"
-    );
-    // interval is how fast the pattern is played
-    // default interval is quarter notes
-    pattern.interval = "4n";
-    pattern.start();
+    track = new Track();
 
     // start the global play button
     Tone.Transport.start();
@@ -92,6 +69,33 @@ const sketch = (s) => {
 
   function modulo(n, m) {
     return ((n % m) + m) % m;
+  }
+
+  class Track {
+    constructor() {
+      this.synth = new Tone.Synth();
+      this.synth.toDestination();
+      this.synth.volume.value = masterVolume;
+
+      this.pattern = new Tone.Pattern(
+        (time, index) => {
+          let note = mapNote(sequence[index], scale);
+          // time is a schedule time in the future. Passing time to synth ensures
+          // the note is played on beat.
+          this.synth.triggerAttackRelease(note, "8n", time);
+          this.currentNote = note;
+        },
+        // to allow us to change the sequence while the program is running, we
+        // pass an array of indicies to Tone.Pattern()
+        // create an array starting at 0, that is same length as sequence.
+        Array.from(sequence.keys()),
+        "up"
+      );
+      // interval is how fast the pattern is played
+      // default interval is quarter notes
+      this.pattern.interval = "4n";
+      this.pattern.start();
+    }
   }
 };
 
